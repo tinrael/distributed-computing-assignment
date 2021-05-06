352379ebae1dc8081b26954b52b61cc751691c2b
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-05-06 17:37:54 +0300

    Create a Film class

---
+++ task1/src/ua/knu/csc/entity/Film.java
@@ -0,0 +1,32 @@
+package ua.knu.csc.entity;
+
+public class Film {
+    private int filmId;
+
+    private String title;
+    private String country;
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
