68f0318e4c48abd566a425de4b88e05fa2a5efab
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-05-06 17:03:52 +0300

    Create an Actor class

---
+++ task1/src/ua/knu/csc/entity/Actor.java
@@ -0,0 +1,32 @@
+package ua.knu.csc.entity;
+
+public class Actor {
+    private int actorId;
+
+    private String forename;
+    private String surname;
+
+    public int getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(int actorId) {
+        this.actorId = actorId;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
