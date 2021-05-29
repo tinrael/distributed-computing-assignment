042b363b791eafe6d79bc6e811b3804d3b455663
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-05-29 17:12:23 +0300

    Update the single-threaded server to be a multithreaded server

--- task1/src/ua/knu/csc/Server.java
+++ task1/src/ua/knu/csc/Server.java
@@ -8,50 +8,80 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Server {
+class Handler extends Thread {
     private Socket socket;
-    private ServerSocket serverSocket;
 
     private PrintWriter printWriter;
     private BufferedReader bufferedReader;
 
-    public void start(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
+    public Handler(Socket socket) throws IOException {
+        this.socket = socket;
 
-        while (true) {
-            socket = serverSocket.accept();
-
-            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            printWriter = new PrintWriter(socket.getOutputStream(), true);
-
-            while (processQuery());
-        }
+        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        printWriter = new PrintWriter(socket.getOutputStream(), true);
     }
 
-    private boolean processQuery() {
+    @Override
+    public void run() {
         try {
-            String query = bufferedReader.readLine();
-            if (query == null) {
-                return false;
+            while (true) {
+                String query = bufferedReader.readLine();
+                if (query == null) {
+                    break;
+                }
+
+                String response = "123" + query + "123";
+
+                printWriter.println(response);
             }
-
-            String response = "123" + query + "123";
-
-            printWriter.println(response);
-
-            return true;
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void start() throws IOException {
+        while (true) {
+            Socket socket = serverSocket.accept();
+            try {
+                Handler handler = new Handler(socket);
+                handler.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+                socket.close();
+            }
         }
     }
 
-    public static void main(String[] args) {
-        Server server = new Server();
+    public void stop() throws IOException {
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server(12345);
         try {
-            server.start(12345);
+            server.start();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                server.stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
