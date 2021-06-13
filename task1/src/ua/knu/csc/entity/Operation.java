1bc183fd915f582cda061a1650253f10f4e4d080
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2021-06-13 18:53:57 +0300

    Add the getOperation(int) function (to get the Operation by the code)

--- task1/src/ua/knu/csc/entity/Operation.java
+++ task1/src/ua/knu/csc/entity/Operation.java
@@ -1,6 +1,11 @@
 package ua.knu.csc.entity;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public enum Operation {
+    UNDEFINED_OPERATION(-1),
+
     ADD_ACTOR(0),
     UPDATE_ACTOR(1),
     DELETE_ACTOR(3),
@@ -11,6 +16,13 @@ public enum Operation {
     DELETE_FILM(7),
     GET_FILM(8);
 
+    private static final Map<Integer, Operation> operations = new HashMap<>();
+    static {
+        for (Operation operation : Operation.values()) {
+            operations.put(operation.code, operation);
+        }
+    }
+
     private final int code;
 
     Operation(int code) {
@@ -19,5 +31,14 @@ public enum Operation {
 
     public int getCode() {
         return code;
+    }
+
+    public static Operation getOperation(int code) {
+        Operation operation = operations.get(code);
+        if (operation == null) {
+            return Operation.UNDEFINED_OPERATION;
+        } else {
+            return operation;
+        }
     }
 }
